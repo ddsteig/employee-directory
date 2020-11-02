0464d2eda8e18bc6324c401895354b769ee902c5
Danny <tekhexrax@yahoo.com>
2020-11-01 21:45:49 -0500

    Added working filter method

--- employeedir/src/components/EmployeeTable.jsx
+++ employeedir/src/components/EmployeeTable.jsx
@@ -42,57 +42,149 @@ const EmployeeTable = (props) => {
     }
     return sortConfig.key === name ? sortConfig.direction : undefined;
   };
-  return (
-    <table className="striped responsive-table">
-      
-      <thead>
-        <tr>
-            <th>Image</th>
-          <th>
-            <button
-              type="button"
-              onClick={() => requestSort('first')}
-              className={getClassNamesFor('first')}
-            >
-              Name
-            </button>
-          </th>
-          <th>
-            <button
-              type="button"
-              onClick={() => requestSort('company')}
-              className={getClassNamesFor('company')}
-            >
-              Company
-            </button>
-          </th>
-          <th>
-            <button
-              type="button"
-              onClick={() => requestSort('age')}
-              className={getClassNamesFor('age')}
-            >
-              Age
-            </button>
-          </th>
-          <th>Phone #</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee.id}>
-            <td><img src={employee.picture} className="card-img-top" alt="..." /></td>
-            <td>{`${employee.first} ${employee.last}`}</td>
-            <td>{employee.company}</td>
-            <td>{employee.age}</td>
-            <td>{employee.phone}</td>
-            <td>{employee.email}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };  
+
+  if (!searchTerm) {
+    return (
+
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Search By First Name"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        </div>
+
+        <table className="striped responsive-table">
+
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>
+                <button
+                  type="button"
+                  onClick={() => requestSort('first')}
+                  className={getClassNamesFor('first')}
+                >
+                  Name
+          </button>
+              </th>
+              <th>
+                <button
+                  type="button"
+                  onClick={() => requestSort('company')}
+                  className={getClassNamesFor('company')}
+                >
+                  Company
+          </button>
+              </th>
+              <th>
+                <button
+                  type="button"
+                  onClick={() => requestSort('age')}
+                  className={getClassNamesFor('age')}
+                >
+                  Age
+          </button>
+              </th>
+              <th>Phone #</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {employees.map((person) => (
+              <tr key={person.id}>
+                <td><img src={person.picture} className="card-img-top" alt="..." /></td>
+                <td>{`${person.first} ${person.last}`}</td>
+                <td>{person.company}</td>
+                <td>{person.age}</td>
+                <td>{person.phone}</td>
+                <td>{person.email}</td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+      </div>
+    )
+  } else {
+  
+    return (
+
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search By First Name"
+              value={searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+
+          <table className="striped responsive-table">
+
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>
+                  <button
+                    type="button"
+                    onClick={() => requestSort('first')}
+                    className={getClassNamesFor('first')}
+                  >
+                    Name
+          </button>
+                </th>
+                <th>
+                  <button
+                    type="button"
+                    onClick={() => requestSort('company')}
+                    className={getClassNamesFor('company')}
+                  >
+                    Company
+          </button>
+                </th>
+                <th>
+                  <button
+                    type="button"
+                    onClick={() => requestSort('age')}
+                    className={getClassNamesFor('age')}
+                  >
+                    Age
+          </button>
+                </th>
+                <th>Phone #</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {employees.filter((person => person.first.toLowerCase().includes(searchTerm.toLocaleLowerCase()))).map(newperson =>(
+                <tr key={newperson.id}>
+                  <td><img src={newperson.picture} className="card-img-top" alt="..." /></td>
+                  <td>{`${newperson.first} ${newperson.last}`}</td>
+                  <td>{newperson.company}</td>
+                  <td>{newperson.age}</td>
+                  <td>{newperson.phone}</td>
+                  <td>{newperson.email}</td>
+                </tr>
+              ))} 
+
+            </tbody>
+          </table>
+        </div>
+      )
+    
+  }
+
 };
 
 export default EmployeeTable;
+
